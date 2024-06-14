93b69ecc3b81b9b06470f0298e1816eeab0332c5
TNALSA <msa1859@naver.com>
2024-06-14 13:39:23 +0900

    coupon type 추가

--- coupon-core/src/main/java/com/travelcurator/couponcore/model/CouponType.java
+++ coupon-core/src/main/java/com/travelcurator/couponcore/model/CouponType.java
@@ -2,5 +2,8 @@ package com.travelcurator.couponcore.model;
 
 public enum CouponType {
     FIRST_COME_FIRST_SERVED, //선착순 쿠폰
-    JAPAN_FIFTY_DISC_COUPON
+    JAPAN_FIFTY_DISC_COUPON,
+    NAGOYA_PACKAGE_COUPON,
+    NATRANG_DISCOUNT_PACKAGE_COUPON,
+    HOKKAIDO_PACKAGE_COUPON
 }
